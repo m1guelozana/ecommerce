33c8a52b3a13394ad65030f7b3dea7a6e18eabd2
miguel-ozana <107505846+miguel-ozana@users.noreply.github.com>
2024-10-18 19:54:17 -0300

    git ignore to resources

--- src/main/java/com/miguelozana/ecommerce/security/JwtTokenProvider.java
+++ src/main/java/com/miguelozana/ecommerce/security/JwtTokenProvider.java
@@ -1,8 +1,54 @@
 package com.miguelozana.ecommerce.security;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtTokenProvider {
-  private final String SECRET_KEY = "jwt-token";
+
+    private final String secretKey;
+
+    public JwtTokenProvider(@Value("${secret.key}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
 }
